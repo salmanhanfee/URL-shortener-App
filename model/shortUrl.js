116f8ede8ae2ca39c3e566544a1d4f3a9621cc58
Salman <salman.hanafee@gslab.com>
2023-01-19 18:49:30 +0530

    first commit

---
+++ model/shortUrl.js
@@ -0,0 +1,22 @@
+const { AuthModuleOptions } = require('@nestjs/passport')
+const mongoose = require('mongoose')
+const shortId = require('shortid')
+
+const shortUrlSchema = new mongoose.Schema({
+    full: {
+        type: String,
+        required: true
+    },
+    short: {
+        type: String,
+        required: true,
+        default: shortId.generate
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+})
+
+module.exports = mongoose.model('shortUrl',shortUrlSchema)
